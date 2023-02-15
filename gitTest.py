429b5830f8f59cf238a16c07d6d0d44f2495aef2
SirFlarflajon <qcwwindsor@outlook.com>
2023-02-14 19:11:48 -0600

    Add prod_arr function.

--- gitTest.py
+++ gitTest.py
@@ -5,8 +5,25 @@ def sum_arr(arr):
     return arr_sum
 
 
+def prod_arr(arr):
+    arr_prod = 1
+    for x in arr:
+        if x == 0:
+            continue
+        arr_prod = x * arr_prod
+    return arr_prod
+
+
 if __name__ == '__main__':
     arr1 = [12, 45, 5]
     arr2 = [2.0, 3.45, 6.375]
+    arr3 = [0, 2, 3, 2]
+    arr4 = [0.25, 0.0, 1.25]
     print(sum_arr(arr1))
+    print(prod_arr(arr1))
     print(sum_arr(arr2))
+    print(prod_arr(arr2))
+    print(sum_arr(arr3))
+    print(prod_arr(arr3))
+    print(sum_arr(arr4))
+    print(prod_arr(arr4))
