2f151c98903258b48f2a6d0289ceca2cf0d0c912
SirFlarflajon <qcwwindsor@outlook.com>
2023-02-14 21:02:29 -0600

    Organize code and add comments to main

--- gitTest.py
+++ gitTest.py
@@ -22,19 +22,22 @@ def swap_arr(arr):
 
 
 if __name__ == '__main__':
+    # Array tests, using both integers and double
     arr1 = [12, 45, 5]
     arr2 = [2.0, 3.45, 6.375]
     arr3 = [0, 2, 3, 2]
     arr4 = [0.25, 0.0, 1.25]
+    # Testing out sum_arr
     print(sum_arr(arr1))
-    print(prod_arr(arr1))
     print(sum_arr(arr2))
-    print(prod_arr(arr2))
     print(sum_arr(arr3))
-    print(prod_arr(arr3))
     print(sum_arr(arr4))
+    # Testing out prod_arr
+    print(prod_arr(arr1))
+    print(prod_arr(arr2))
+    print(prod_arr(arr3))
     print(prod_arr(arr4))
-
+    # Testing out swap_arr
     print(swap_arr(arr1))
     print(swap_arr(arr2))
     print(swap_arr(arr3))
